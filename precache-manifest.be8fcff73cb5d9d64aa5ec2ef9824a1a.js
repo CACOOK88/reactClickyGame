c15402b374d62d29f485005ccc1966df227e3089
Chad Cook <chadcookcoding@gmail.com>
2019-02-24 08:09:35 -0500

    Updates

---
+++ precache-manifest.be8fcff73cb5d9d64aa5ec2ef9824a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06b2e5f65613f39ea20293f0e8688819",
+    "url": "/reactClickyGame/static/media/weights.06b2e5f6.jpeg"
+  },
+  {
+    "revision": "d2b0f2ce6bf446e6825b",
+    "url": "/reactClickyGame/static/js/runtime~main.d2b0f2ce.js"
+  },
+  {
+    "revision": "37e4ccaf95f23030827b",
+    "url": "/reactClickyGame/static/js/main.37e4ccaf.chunk.js"
+  },
+  {
+    "revision": "3a096312610b412571cf",
+    "url": "/reactClickyGame/static/js/2.3a096312.chunk.js"
+  },
+  {
+    "revision": "37e4ccaf95f23030827b",
+    "url": "/reactClickyGame/static/css/main.53d0391d.chunk.css"
+  },
+  {
+    "revision": "8b12ccb2a024c22851ce816769447bf3",
+    "url": "/reactClickyGame/index.html"
+  }
+];
